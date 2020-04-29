273f960f3248149087dde767f04dc62f9857fa51
ipraveenjain <ipraveenjain@gmail.com>
2020-04-29 15:46:05 +0530

    de

--- src/main/java/pbj/springmvc/AzspcloudApplication.java
+++ src/main/java/pbj/springmvc/AzspcloudApplication.java
@@ -3,9 +3,12 @@ package pbj.springmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@ComponentScan("pbj.springmvc")
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class AzspcloudApplication {
 
 	public static void main(String[] args) {
